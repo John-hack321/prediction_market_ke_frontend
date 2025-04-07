f29f49c379490514cd058538eeefd755214447ed
scorpion_wrist <johtieno6@gmail.com>
2025-04-07 07:27:12 +0300

    this is the initial initialization of the project itself

---
+++ polymarket_ke/src/components/navBar.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+
+function NavBar() {
+  return (
+    <div>
+      <div>this is a test to see if the nav bar is working as expected</div>
+    </div>
+  );
+}
+
+export default NavBar;
+
+
+
